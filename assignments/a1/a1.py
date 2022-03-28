e2c1cb7de8831f206d38454047c9b1e09550f42f
madhurmishra09 <madhur.mishra@students.mq.edu.au>
2022-03-28 13:33:57 +1100

    week 4 workshop

--- assignments/a1/a1.py
+++ assignments/a1/a1.py
@@ -6,7 +6,13 @@ nltk.download('gutenberg')
 from sklearn.feature_extraction.text import TfidfVectorizer
 import collections
 
+
+from nltk.stem import PorterStemmer 
+
+
+
 # Task 1 (2 marks)
+import collections
 def count_pos(document, pos):
     """Return the number of occurrences of words with a given part of speech. To find the part of speech, use 
     NLTK's "Universal" tag set. To find the words of the document, use NLTK's sent_tokenize and word_tokenize.
@@ -15,10 +21,27 @@ def count_pos(document, pos):
     >>> count_pos('austen-sense.txt', 'VERB')
     25074
     """
-    return 0
 
+    """   stopwords = nltk.corpus.stopwords.words('english')
+    
+    """
+    stopwords = nltk.corpus.stopwords.words('english')
+    raw = nltk.corpus.gutenberg.raw(document)
+    nltk.download('averaged_perceptron_tagger')
+    import collections
+    emma_sents = [nltk.word_tokenize(s) for s in nltk.sent_tokenize(nltk.corpus.gutenberg.raw('austen-emma.txt'))]
+    emma_tagged_sents = [nltk.pos_tag(s, tagset="universal") for s in emma_sents]
+    pos = []
+    for s in emma_tagged_sents:
+        for w in s:
+            pos.append(w[1])
+    counter = collections.Counter(pos)
+    counter.most_common(5)
+    return counter 
+
+""""" comment start 
 # Task 2 (2 marks)
-def get_top_stem_bigrams(document, n):
+def get_top_stem_bigrams(document, n): comment end """ 
     """Return the n most frequent bigrams of stems. Return the list sorted in descending order of frequency.
     The stems of words in different sentences cannot form a bigram. To stem a word, use NLTK's Porter stemmer.
     To find the words of the document, use NLTK's sent_tokenize and word_tokenize.
@@ -27,11 +50,11 @@ def get_top_stem_bigrams(document, n):
     >>> get_top_stem_bigrams('austen-sense.txt',4)
     [(',', 'and'), ('.', "''"), (';', 'and'), (',', "''")]
     """
-    return []
+  """ comment start  return []
 
 
 # Task 3 (2 marks)
-def get_same_stem(document, word):
+def get_same_stem(document, word): comment end """
     """Return the list of words that have the same stem as the word given, and their frequencies. 
     To find the stem, use NLTK's Porter stemmer. To find the words of the document, use NLTK's 
     sent_tokenize and word_tokenize. The resulting list must be sorted alphabetically.
@@ -40,7 +63,16 @@ def get_same_stem(document, word):
     >>> get_same_stem('austen-sense.txt','respect')[:5]
     [('respect', 22), ('respectability', 1), ('respectable', 14), ('respectably', 1), ('respected', 3)]
     """
-    return []
+   ''' sent_tokens = [word_tokenize(s) for s in sent_tokenize(nltk.corpus.gutenberg.raw(document))]
+    stems= set()
+    porter= nltk.PorterStemmer()
+    for s in sent_tokens:
+        stemmed= porter.stem(word)
+        if stemmed == porter.stem(s):
+            stems.add(s)
+        else:
+            return[0,0]
+    return [stems,0]
 
 # Task 4 (2 marks)
 def most_frequent_after_pos(document, pos):
@@ -66,10 +98,13 @@ def get_word_tfidf(text):
     >>> get_word_tfidf('Brutus is a honourable person')
     [('brutus', 0.8405129362379974), ('honourable', 0.4310718596448824), ('person', 0.32819971943754456)]
     """
-    return []
 
+
+    return [] 
+    '''
 
 # DO NOT MODIFY THE CODE BELOW
 if __name__ == "__main__":
     import doctest
     doctest.testmod(optionflags=doctest.ELLIPSIS)
+    
